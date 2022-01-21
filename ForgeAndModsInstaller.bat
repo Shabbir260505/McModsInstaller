2bc2f275be280060cbd7e28eae02f6688e3a02f8
Shabbir Ezzy <shabbir.ezzy.official@gmail.com>
2022-01-21 15:50:33 +0530

    Nothing Much

--- ForgeAndModsInstaller.bat
+++ ForgeAndModsInstaller.bat
@@ -1,3 +1,4 @@
 @echo off
 echo Downloading Files! Please Wait :)
+wget -P .\temp https://github.com/Shabbir260505/McModsInstaller/blob/main/files/mods/Skytils.jar?raw=true
 java -jar .\files\forgeinstaller\forge-1.8.9-11.15.1.2318-1.8.9-installer.jar
